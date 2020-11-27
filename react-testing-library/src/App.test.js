685eec345def86780e0c0a52c8f3439cf1567227
liudmyla <liudmyla@teemyco.com>
2020-11-27 23:22:27 +0200

    add example using queryBy - searching for non existing element

--- react-testing-library/src/App.test.js
+++ react-testing-library/src/App.test.js
@@ -4,11 +4,7 @@ import App from './App';
 describe("App", () => {
   it ("renders App component", () => {
     render(<App />);
-    expect(screen.getByText("Search:")).toBeInTheDocument();
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('search text...')).toBeInTheDocument();
-    expect(screen.getByAltText('search image')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+    expect(screen.queryByText(/Searches for React/)).toBeNull();
+    
   })
 })
