0b65152739120e967100143333ce573880cae1a5
liudmyla <liudmyla@teemyco.com>
2020-11-28 18:16:06 +0200

    add example for fireEvent - testing user intreaction

--- react-testing-library/src/App.test.js
+++ react-testing-library/src/App.test.js
@@ -1,17 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import App from './App';
 
 describe("App", () => {
   it ("renders App component", async () => {
     render(<App />);
-    // expect(screen.queryByText(/Searches for React/)).toBeNull();
-    expect(screen.queryByText(/Logged in as/i)).toBeNull();
-    screen.debug();
-    expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument();
-    screen.debug();
-    expect(screen.getByAltText(/search image/i)).toHaveClass('image');
-    expect(screen.getByLabelText(/search/i)).not.toBeRequired();
-    expect(screen.getByLabelText(/search/i)).toBeEmpty();
-    expect(screen.getByLabelText(/search/i)).toHaveAttribute('id');
+    await screen.findByText(/Logged in as/i);
+    expect(screen.queryByText(/Searches for React/)).toBeNull();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'React'}
+    });
+    expect(screen.queryByText(/Searches for React/)).toBeInTheDocument();
+  })
+});
+
+describe("events", () => {
+  it("checkbox click", () => {
+    const handleChange = jest.fn();
+    const { container } = render(<input type="checkbox" onChange={handleChange} />);
+    const checkbox = container.firstChild;
+    expect(checkbox).not.toBeChecked();
+    fireEvent.click(checkbox);
+    expect(handleChange).toHaveBeenCalledTimes(1);
+    //or
+    expect(checkbox).toBeChecked();
+
+  });
+  it("input focus", () => {
+    const { getByTestId } = render(
+      <input type="text" data-testid="simple-id" />
+    );
+    const input = getByTestId("simple-input");
+    expect(input).not.toHaveFocus();
+    input.focus();
+    expect(input).toHaveFocus();
   })
 })
