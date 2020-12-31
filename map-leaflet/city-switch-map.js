ccf2cd873596a63735aca4a68eb27fb4ed94ce34
mila <--local>
2020-12-31 22:28:23 +0200

    [map-leaflet] add changing cities on map

---
+++ map-leaflet/city-switch-map.js
@@ -0,0 +1,21 @@
+let map = null;
+let marker = null;
+
+export function getMap(position, tooltip) {
+    if (map === null) {
+        map = L.map('map').setView(position, 15);
+    } else {
+        map.flyTo(position);
+    }
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+
+    if (marker) {
+        map.removeLayer(marker);
+    };
+
+    marker = new L.Marker(position).addTo(map).bindPopup(tooltip).openPopup();
+}
---
+++ map-leaflet/city-switch-script.js
@@ -0,0 +1,33 @@
+import { getMap } from './city-switch-map.js';
+
+const $cities = document.getElementById('cities');
+
+;(async () => {
+    const response = await fetch('./db/cities.json');
+
+    console.log(response);
+    const cities = await response.json();
+
+    for (const city in cities) {
+        const $button = document.createElement('button');
+
+        $button.textContent = city;
+
+        const { lat, lon } = cities[city];
+
+        $button.dataset.city = city;
+        $button.dataset.lat = lat;
+        $button.dataset.lon = lon;
+
+        $cities.append($button);
+    }
+})();
+
+$cities.addEventListener('click', ({ target }) => {
+    if (target.tagName != 'BUTTON') return;
+
+    const { city, lat, lon } = target.dataset;
+    const position = [lat, lon];
+
+    getMap(position, city);
+});
