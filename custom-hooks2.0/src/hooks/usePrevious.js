6cf5dbea9b55625c2dfc7938345c8dd9b27fe0fd
mila <--local>
2021-02-07 14:30:44 +0200

    [custom-hooks2.0] add usePreview hook

--- custom-hooks2.0/src/hooks/usePrevious.js
+++ custom-hooks2.0/src/hooks/usePrevious.js
@@ -1,5 +1,15 @@
-function usePrevious() {
+import { useRef, useEffect } from 'react';
 
+function usePrevious(value) {
+  const currentRef = useRef();
+
+  useEffect(() => {
+    if (currentRef.current !== value) {
+      currentRef.current = value;
+    }
+  }, [value]);
+  
+  return currentRef.current;
 };
 
 export default usePrevious;
