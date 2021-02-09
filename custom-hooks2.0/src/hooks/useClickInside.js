0080df7239ec04d73a41bdd7f0fea59aa2b73b02
mila <--local>
2021-02-10 00:45:43 +0200

    [custom-hooks2.0] add useClickInside hook

--- custom-hooks2.0/src/hooks/useClickInside.js
+++ custom-hooks2.0/src/hooks/useClickInside.js
@@ -1,6 +1,20 @@
 import { useEffect } from 'react';
 
 function useClickInside(ref, cb) {
+  function callback(e) {
+    if (ref.current && ref.current.contains(e.target)) {
+      cb();
+    }
+  }
+
+  useEffect(() => {
+    const elRef = ref.current;
+    if (elRef) {
+      elRef.addEventListener('click', callback);
+    }
+
+    return () => elRef && elRef.removeEventListener('click', callback);
+  }, []);
 
 };
 
