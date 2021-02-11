f3d1fa389a28dbdeb16a4b7193335c3c4a794eb0
mila <--local>
2021-02-12 01:16:03 +0200

    [custom-hooks2.0] add use outside click hook

--- custom-hooks2.0/src/hooks/useClickOutside.js
+++ custom-hooks2.0/src/hooks/useClickOutside.js
@@ -1,4 +1,15 @@
-function useClickOutside() {
+import { useEffect } from 'react';
 
+function useClickOutside(ref, cb) {
+  function callback(e) {
+    if (ref.current && !ref.current.contains(e.target)) {
+      cb();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', callback);
+    return () => document.removeEventListener('click', callback);
+  }, []);
 };
 export default useClickOutside;
