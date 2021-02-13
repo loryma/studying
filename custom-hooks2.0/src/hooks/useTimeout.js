421b9cde0afc0ad2ca37b3b12af820ff230668a8
mila <--local>
2021-02-13 23:29:22 +0200

    [custom-hook2.0] add useTimeout hook

--- custom-hooks2.0/src/hooks/useTimeout.js
+++ custom-hooks2.0/src/hooks/useTimeout.js
@@ -1,4 +1,22 @@
-function useTimout() {
+import { useEffect, useRef } from 'react';
 
+function useTimeout(callback, delay) {
+  const cbRef = useRef();
+
+  useEffect(() => {
+    if(cbRef.current !== callback) {
+      cbRef.current = callback;
+    }
+  }, [callback]);
+
+  useEffect(() => {
+    let timerId;
+
+    if (cbRef.current && Number.isInteger(delay)) {
+      timerId = setTimeout(cbRef.current, delay);
+    }
+
+    return () => clearTimeout(timerId);
+  }, [delay])
 };
 export default useTimeout;
