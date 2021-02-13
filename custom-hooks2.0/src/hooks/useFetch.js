64b9fff5517b6e7eacf92b552759285588117069
mila <--local>
2021-02-13 15:14:17 +0200

    [custom=hooks2.0] add useFetch hook

--- custom-hooks2.0/src/hooks/useFetch.js
+++ custom-hooks2.0/src/hooks/useFetch.js
@@ -1,5 +1,17 @@
-function useFetch() {
+import { useEffect, useState } from 'react';
 
+function useFetch({url, options={}}) {
+  const [response, setResponse] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch(url, options)
+      .then(res => res.json())
+      .then(res => setResponse(res))
+      .catch(err => setError(err));
+  }, [url]);
+
+  return {response, error};
 };
 
 export default useFetch;
