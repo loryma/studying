d7cd4eec29ee47f14400c2fa76e8e1e966ba9054
mila <--local>
2021-02-14 16:56:37 +0200

    [custom-hooks2.0] add useInterval hook

--- custom-hooks2.0/src/hooks/useInterval.js
+++ custom-hooks2.0/src/hooks/useInterval.js
@@ -1,4 +1,23 @@
-function useInterval() {
-  
+import { useEffect, useRef } from 'react';
+
+function useInterval(callback, delay) {
+  const cbRef = useRef();
+
+  useEffect(() => {
+    if ( cbRef.current !== callback ) {
+      cbRef.current = callback;
+    }
+  }, [callback]);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (cbRef.current && Number.isInteger(delay)) {
+      setInterval(cbRef.current, delay);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [delay]);
 };
+
 export default useInterval;
