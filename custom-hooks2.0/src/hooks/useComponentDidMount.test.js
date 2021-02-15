e072ca3df1f6659aa6378124ec80ae3fe9c3cef2
mila <--local>
2021-02-16 00:48:12 +0200

    [custom-hooks2.0] fix test for useComponentDidMount hook

--- custom-hooks2.0/src/hooks/useComponentDidMount.test.js
+++ custom-hooks2.0/src/hooks/useComponentDidMount.test.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { act } from "@testing-library/react";
 import { renderHook } from '@testing-library/react-hooks';
 import useComponentDidMount from './useComponentDidMount';
 
 describe('useComponentDidMounHook', () => {
   const spyFunction = jest.fn();
 
-  test ('should callback on mount', () => {
-    const { rerender } = renderHook(() => (spyFunction));
+  test ('should callback callback on mount', () => {
+    renderHook(() => useComponentDidMount(spyFunction));
+
+    expect(spyFunction).toHaveBeenCalledTimes(1);
+
+    renderHook(() => useComponentDidMount(spyFunction));
+
+    expect(spyFunction).toHaveBeenCalledTimes(2);
+
+  });
+
+  test ('should call callback only on mount and not on next renders', () => {
+    const { rerender } = renderHook(() => useComponentDidMount(spyFunction));
 
     expect(spyFunction).toHaveBeenCalledTimes(1);
 
     rerender();
 
-    expect(spyFunction).toHaveBeenCalledTimes(2);
+    expect(spyFunction).toHaveBeenCalledTimes(1);
 
-  })
+  });
 })
