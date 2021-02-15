da35aed76109df8131d32b5d28d5325190a8f41d
mila <--local>
2021-02-16 01:00:02 +0200

    [custom-hooks2.0] add useComponentWilUnmount hook

--- custom-hooks2.0/src/hooks/useComponentWillUnmount.js
+++ custom-hooks2.0/src/hooks/useComponentWillUnmount.js
@@ -3,6 +3,9 @@ import useComponentDidMount from "./useComponentDidMount";
 import { useEffect } from 'react';
 
 function useComponentWillUnmount(callback) {
+  useEffect(() => {
+    return callback && callback();
+  }, []);
 };
 
 export default useComponentWillUnmount;
